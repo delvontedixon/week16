5cb7aba4151ec7ceff71c5cc84abbc1214a0c27a
delvontedixon <delvontedixon@yahoo.com>
2024-02-16 19:42:08 -0500

    button adjustment

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,14 +1,27 @@
 import { Col } from "react-bootstrap";
 import Badge from "./Badge.js";
+import { useState } from "react";
+
 function UserProfile({ user }) {
   const { name, avatar, phone, id, teams } = user;
+  const [badgeStatus, setBadgeStatus] = useState(false);
 
+  function handleClick() {
+    console.log("button to show teams was clicked: ", badgeStatus);
+    setBadgeStatus(!badgeStatus);
+    console.log(badgeStatus);
+  }
   return (
     <Col key={id} className="p-2 text-center border border-1 rounded-4 m-1">
       <img src={avatar} alt={name} className="img-fluid rounded-circle Image" />
       <h1 className="fs-4">{name}</h1>
       <p>{phone}</p>
-      <Badge teams={teams} />
+      <Button variant="secondary" onClick={() => handleClick()}>
+        Show Teams
+      </Button>
+      <div className={badgeStatus ? "" : "d-none"}>
+        <Badge teams={teams} />
+      </div>
     </Col>
   );
 }
