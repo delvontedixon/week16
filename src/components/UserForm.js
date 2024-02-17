2018bfc0c03ad351b4092e2f3bb9e3648ca515b5
delvontedixon <delvontedixon@yahoo.com>
2024-02-17 15:03:38 -0500

    attempting to add form

---
+++ src/components/UserForm.js
@@ -0,0 +1,16 @@
+
+
+function Form() {
+    return (
+<div class="row">
+    <div class="col">
+      <form>
+          <div class="form-group">
+                <label for="exampleInputEmail1">Coach Name</label>
+                <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Name & City">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+</div>
+);
+}
