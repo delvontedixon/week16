947b15ee729b69bbc0e871c2040c7e4bdb21581e
delvontedixon <delvontedixon@yahoo.com>
2024-02-25 12:24:00 -0500

    router nav

--- src/components/routing.js
+++ src/components/routing.js
@@ -1,15 +1,21 @@
 import UserProfile from "./components/UserProfile";
 import Background from "./Background";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 
 export default function Routing(props) {
   return (
-    <Routes>
-      {/* if the route is to /home, it will show the Home page */}
-      <Route path="/userprofile" element={<UserProfile />} />
-      {/* if the route is to /userprofile it will show the main page*/}
-      <Route path="/background" element={<Background />} />
-      {/* if the route is to /background it will show the background page */}
-    </Routes>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* if the route is to /home, it will show the Home page */}
+          <Route path="/userprofile" element={<UserProfile />} />
+          {/* if the route is to /userprofile it will show the main page*/}
+          <Route path="/background" element={<Background />} />
+          {/* if the route is to /background it will show the background page */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
