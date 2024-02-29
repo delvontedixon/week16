481df23e31e951ae43dd5369772c99601854268f
delvontedixon <delvontedixon@yahoo.com>
2024-02-29 09:00:08 -0500

    table for background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,5 +1,44 @@
 function Background(props) {
-  return <div></div>;
+  return (
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Teams</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Nick</td>
+            <td>Saban</td>
+            <td>BAMA & LSU</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Urban</td>
+            <td>Meyer</td>
+            <td>FLORIDA & OHIO</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Phil</td>
+            <td>Jackson</td>
+            <td>BULLS & LAERS</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Coach</td>
+            <td>Prime</td>
+            <td>JSU & COLORADO</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Background;
